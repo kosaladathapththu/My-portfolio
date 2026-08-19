a63700b5299238707287972639839c570250f7ec
A.M.Kosala Dhaneshwara Athapaththu <kosalaathapaththu1234@gmail.com>
2026-08-19 10:34:39 +0530

    Remove Kosala Portfolio theme files including screenshot and style.css

--- wordpress-theme/kosala-portfolio/front-page.php
+++
@@ -1,3 +0,0 @@
-<?php
-if (!defined('ABSPATH')) { exit; }
-echo kosala_portfolio_document();
--- wordpress-theme/kosala-portfolio/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!defined('ABSPATH')) { exit; }
-get_header();
-if (have_posts()) { while (have_posts()) { the_post(); the_title('<h1>', '</h1>'); the_content(); } }
-get_footer();
